2135e9f8d9ffc6333312a2196aa6b3fd95dafba0
Sam Edelstein <sedelstein@ithaca.edu>
2021-04-30 16:57:45 -0400

    cards

--- src/Section4.js
+++ src/Section4.js
@@ -41,6 +41,7 @@ class Section4 extends React.Component {
 
           <div className="container-fluid">
             <div className="row">
+              
               <div className="col-md-6 intro d-flex justify-content-center">
               <div className="row intro-text">
                 <h1 className="col-md-12">Minimalism</h1>
@@ -53,6 +54,37 @@ class Section4 extends React.Component {
                 <img className='img-fluid' src={modernGraphic} />
               </div>
             </div>
+
+
+            {/* CARDS */}
+            <div class="card-deck">
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+
+            
           </div>
 
         </div>
