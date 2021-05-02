8977933ab6ab9930f051469228e8b6dffc3ea46e
Sean McQuillan <mcquill99@gmail.com>
2021-05-02 15:53:55 -0400

    Added initial splash page

---
+++ src/Intro.js
@@ -0,0 +1,23 @@
+import React from "react";
+import frameOne from "./images/frameOne.png";
+import backgroundOne from "./images/win31blur.png";
+
+class Intro extends React.Component {
+    render() {
+      return (
+        <div className="section intro">
+            <div className="background introBackground">
+            <p className="anim-typewriter type">
+                Welcome, to the History of the World Wide Web.
+            </p>
+
+            </div>
+        </div>
+      );
+}
+}
+
+
+
+
+export default Intro;
