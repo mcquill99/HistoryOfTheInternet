40f9a5e425df95e4583f239888755fd0ae41ef1e
deverydickson <54859654+deverydickson@users.noreply.github.com>
2021-05-03 04:02:42 -0400

    attempted div replacement

--- src/Section3.js
+++ src/Section3.js
@@ -3,6 +3,7 @@ import frameThree from "./images/frameThree.png";
 import backgroundThree from "./images/mac-leopard-blur.png";
 import logoEraThree from "./images/logoEraThree.png";
 import marathon from "./images/marathon.jpeg";
+import $ from 'jquery';
 
 class Section3 extends React.Component {
   render() {
@@ -19,16 +20,27 @@ class Section3 extends React.Component {
           </div>
           </div>
           <div className="whiteHolder">
-            <div className="feed">
+            <div className="feed" id="loginPageFeed">
               <img src={marathon} width={550} height={340}/>
               <br></br>
               <div className="socialMediaDescription">
               By the late 2000s, the number of users on social media had skyrocketed. Login to explore this era.
               </div>
             </div>
+
+
+            
+            <div className="feed" id="loggedInFeed" style="display:none;">
+              alternate div, trying to switch to this. delete this one if you delete anything, the feed with id loginPageFeed is working fine on its own
+
+
+
+
+
+            </div>
             <div className="sidebar">
               <p>Sign into account</p>
-              <button class="loginButton">Log in</button>
+              <button id="loginButton">Log in</button>
               
             </div>
             
@@ -43,4 +55,24 @@ class Section3 extends React.Component {
   }
 }
 
+/* this is the jquery i was trying to use to switch the div content */
+$('#loginButton').on('click', function(){
+  if($('#loginPageFeed').css('display')!='none'){
+    $('#loggedInFeed').html('Logged in feed').show().siblings('div').hide();
+  }else if($('#loggedInFeed').css('display')!='none'){
+    $('#loginPageFeed').show().siblings('div').hide();
+  }
+});
+
+
+
+
+
+
+
+
+
+
 export default Section3;
+
+
